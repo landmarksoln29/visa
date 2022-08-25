a8ed61c153236ac82d3d3e605574f141cf7ddadd
Jonni Asu <jonniasumadu@gmail.com>
2022-08-25 06:41:29 -0400

    Updated LFC Staffs with Head of Fitness

--- liverpoolstaffs.sh
+++ liverpoolstaffs.sh
@@ -1,4 +1,5 @@
 # Liverpool Staff
-1.Jurgen Klopp    - Head Coach
-2.John Achterberg - Goalkeeping Coach
-3.Jim Moxon       - Club Coach
+1.Jurgen Klopp          - Head Coach
+2.John Achterberg       - Goalkeeping Coach
+3.Jim Moxon             - Club Coach
+4.Andreas Schlumberger  - Head of Fitness
